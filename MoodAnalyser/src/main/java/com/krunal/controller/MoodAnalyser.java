79a00a1957f8e0f9fde840e12e59f79e98d513cc
kunallad54 <kunallad5473@gmail.com>
2021-06-30 17:59:48 +0530

    Refactored Code

--- MoodAnalyser/src/main/java/com/krunal/controller/MoodAnalyser.java
+++ MoodAnalyser/src/main/java/com/krunal/controller/MoodAnalyser.java
@@ -1,12 +1,35 @@
+/***********************************************************************************************
+ *
+ * Purpose : To read message from user and return sad if message contains sad else happy
+ *
+ * @authtor Krunal Lad
+ * @Since 30-06-2021
+ *
+ **********************************************************************************************/
+
 package com.krunal.controller;
 
 public class MoodAnalyser {
+    private String message;
 
-    public String analyseMood(String message) {
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Purpose : To check message and return accordingly
+     * @return sad if message contains sad else false
+     */
+    public String analyseMood() {
+
         if(message.contains("sad"))
             return "SAD";
         else
             return "HAPPY";
+
     }
 
 }
--- MoodAnalyser/src/test/java/com/krunal/controller/MoodAnalyserTest.java
+++ MoodAnalyser/src/test/java/com/krunal/controller/MoodAnalyserTest.java
@@ -1,3 +1,7 @@
+/**
+ * Purpose : To perform JUnit Testing on Mood Analyzer program
+ */
+
 package com.krunal.controller;
 
 import org.hamcrest.CoreMatchers;
@@ -6,17 +10,18 @@ import org.junit.Test;
 
 public class MoodAnalyserTest {
 
+
     @Test
     public void givenMessage_ToMoodAnalyser_ShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String message = moodAnalyser.analyseMood("I am in sad mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
+        String message = moodAnalyser.analyseMood();
         Assert.assertThat(message, CoreMatchers.is("SAD"));
     }
 
     @Test
     public void givenMessage_ToMoodAnalyser_ShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String message = moodAnalyser.analyseMood("I am in any mood");
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
+        String message = moodAnalyser.analyseMood();
         Assert.assertThat(message,CoreMatchers.is("HAPPY"));
     }
 
